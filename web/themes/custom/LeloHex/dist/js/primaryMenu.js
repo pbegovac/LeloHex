0423d034212d7ce73bc7d197a3875153a95c9321
pavaobegi@gmail.com <pavao.begovac@lelo.com>
2023-04-04 16:28:46 +0200

    adding a vanilla JS version

--- web/themes/custom/LeloHex/dist/js/primaryMenu.js
+++ web/themes/custom/LeloHex/dist/js/primaryMenu.js
@@ -1,40 +1,87 @@
-((Drupal, $) => {
-  const navList = $(".primary-nav__menu-link-inner--level-1");
-  const submenu = $(".primary-nav__menu--level-2");
-  const logIn = submenu.eq(0);
-  const languages = submenu.eq(1);
-  const languageOptions = $(".primary-nav__menu-link-inner--level-2");
-  const language = navList.eq(1);
+// ((Drupal, $) => {
+//   const navList = $(".primary-nav__menu-link-inner--level-1");
+//   const submenu = $(".primary-nav__menu--level-2");
+//   const logIn = submenu.eq(0);
+//   const languages = submenu.eq(1);
+//   const languageOptions = $(".primary-nav__menu-link-inner--level-2");
+//   const language = navList.eq(1);
 
-  logIn.hide();
-  languages.hide();
+//   logIn.hide();
+//   languages.hide();
+
+//   //prevent reloading on hitting inner span
+//   languageOptions.click((e) => {
+//     e.preventDefault();
+//   });
+
+//   // Opening of submenu
+//   language.click((e) => {
+//     languages.show();
+//     e.preventDefault();
+//   });
+
+//   Drupal.behaviors.lelohexHideMenu = {
+//     attach: (context) => {
+//       //close with outside and inner li click
+//       $(document).mouseup((e) => {
+//         if (!language.is(e.target)) {
+//           e.preventDefault();
+//           languages.hide();
+//         }
+//       });
+//       //close with escape
+//       $(document).keyup((e) => {
+//         if (e.keyCode == 27) {
+//           languages.hide();
+//         }
+//       });
+//     },
+//   };
+// })(Drupal, jQuery);
+
+((Drupal) => {
+  const navList = document.querySelectorAll(
+    ".primary-nav__menu-link-inner--level-1"
+  );
+  const submenu = document.querySelectorAll(".primary-nav__menu--level-2");
+  const logIn = submenu[0];
+  const languages = submenu[1];
+  const languageOptions = document.querySelectorAll(
+    ".primary-nav__menu-link-inner--level-2"
+  );
+  const language = navList[1];
+
+  logIn.style.display = "none";
+  languages.style.display = "none";
 
   //prevent reloading on hitting inner span
-  languageOptions.click((e) => {
-    e.preventDefault();
+  languageOptions.forEach((option) => {
+    option.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
   });
 
   // Opening of submenu
-  language.click((e) => {
-    languages.show();
+  language.addEventListener("click", (e) => {
+    languages.style.display = "flex";
     e.preventDefault();
   });
 
   Drupal.behaviors.lelohexHideMenu = {
     attach: (context) => {
       //close with outside and inner li click
-      $(document).mouseup((e) => {
-        if (!language.is(e.target)) {
+      document.addEventListener("mouseup", (e) => {
+        if (!language.contains(e.target)) {
           e.preventDefault();
-          languages.hide();
+          languages.style.display = "none";
         }
       });
       //close with escape
-      $(document).keyup((e) => {
-        if (e.keyCode == 27) {
-          languages.hide();
+      document.addEventListener("keyup", (e) => {
+        if (e.key == "Escape") {
+          languages.style.display = "none";
         }
       });
     },
   };
-})(Drupal, jQuery);
+})(Drupal);
